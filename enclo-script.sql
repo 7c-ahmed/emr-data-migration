a9f4e11b0f1a379203096fdacefae1ed1adf354d
7c-ahmed <muhammadahmedmallick@sevencells.com>
2024-05-22 21:49:26 +0500

    enclo script for future added

---
+++ enclo-script.sql
@@ -0,0 +1,47 @@
+INSERT INTO subscription_template 
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00637-80e3-45a9-a01d-fe9164db84dc', '1', 1, 0, '(Weeks 1-4)', '113ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '113ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00638-80e3-45a9-a01d-fe9164db84dc', '2', 0, 0, '(Weeks 5-8)', '111ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '111ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00639-80e3-45a9-a01d-fe9164db84dc', '3', 0, 0, '(Weeks 9-12)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00640-80e3-45a9-a01d-fe9164db84dc', '4', 1, 0, '(Weeks 13-16)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00641-80e3-45a9-a01d-fe9164db84dc', '5', 0, 0, '(Weeks 17-20)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00642-80e3-45a9-a01d-fe9164db84dc', '6', 0, 0, '(Weeks 21-24)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00643-80e3-45a9-a01d-fe9164db84dc', '7', 1, 0, '(Weeks 25-28)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00644-80e3-45a9-a01d-fe9164db84dc', '8', 0, 0, '(Weeks 29-32)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00645-80e3-45a9-a01d-fe9164db84dc', '9', 0, 0, '(Weeks 33-36)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00646-80e3-45a9-a01d-fe9164db84dc', '10', 0, 0, '(Weeks 37-40)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00647-80e3-45a9-a01d-fe9164db84dc', '11', 0, 0, '(Weeks 41-44)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
+
+INSERT INTO subscription_template
+(id, month_number, need_blood_test, need_follow_up, week, variant_id, recomended_variant_id, product_id)
+VALUES('55f00648-80e3-45a9-a01d-fe9164db84dc', '12', 0, 0, '(Weeks 45-48)', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', '114ba87b-dfc1-4f0d-94a5-2d835c95fe6d', 'feea4e93-c125-4c24-a235-198ad7aeca74');
